1859e1459c2c5828d4d25cd70ec3c7a1ac211f7e
Eliezer Queiroz <eliezer.lima@betrybe.com>
2023-09-13 12:01:40 -0300

    adicionando comentários

--- buziosGame.js
+++ buziosGame.js
@@ -1,3 +1,8 @@
+// Tabela de advinhação DEV
+// Os ODevs são estados probabilisticos da sua energia DEV
+// Existem 16 Odevs principais, descubra o seu
+// 
+
 // Criar 4 variáveis que representam búzios
 let buzio1 = Math.floor(Math.random() * 2); // Gerar um valor aleatório entre 0 e 1
 let buzio2 = Math.floor(Math.random() * 2);
@@ -15,3 +20,8 @@ for (let buzio = 0; buzio < tabuleiro.length; buzio += 1) {
 }
 
 console.log(tabuleiro);
+
+// criar a ideia de "rodada", cada rodada um DEV joga os búzos para descobrir o seu caminho DEV 
+// e depois faz uma pergunta e joga novamente para receber uma resposta
+
+//criar a possibilidade de aumentar a quantidade de búzios no jogo
